162539d7014997d5efe00167a7679fcbfbd105a2
maxim1986y <maximvy@gmail.com>
2026-06-25 15:01:20 +0000

    added env file, network, etc 3

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-test
+Установка:
+git clone https://gitverse.ru/maxim1986y/test_em https://gitverse.ru/maxim1986y/test_em
+cd ./test_em
+Оредактируйте файл .env, указав предпочтительные имена контейнеров
+docker-compose up -d
+
+Проверка работоспособности:
+curl http://localhost
+Ожидаемый ответ:
+"Hello from Effective Mobile!"
+
+Удаление:
+docker-compose down --rmi all
--- backend/app.py
+++ backend/app.py
@@ -6,10 +6,11 @@ class MyHandler(SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
+        #Выводим сообщение при обращении на 8080 порт
         self.wfile.write(b"Hello from Effective Mobile! \n")
 
 if __name__ == "__main__":
     # Serve on all interfaces (0.0.0.0) inside the container
     server = HTTPServer(('0.0.0.0', 8080), MyHandler)
-    print("Hello from Effective Mobile!")
+    print("Server started")
     server.serve_forever()
